e6e35b65807099807374cab3e7338b7f312aeb1b
山下拓真 <yamashitatakushin@MacBook-Pro.local>
2021-08-31 22:08:25 +0900

    volatile

---
+++ signal/global-var.c
@@ -0,0 +1,24 @@
+#include <signal.h>
+#include <stdio.h>
+#define MAX 1000000000
+
+static long long counter = 0;
+
+void handler(int sig){
+    printf("%lld\n", counter);
+}
+
+int main(void){
+    long long i;
+    signal(SIGINT, handler);
+
+    /* ループ中にカウンターを参照するかもしれない
+     * counterを別のレジスタに入れてループ(しながらインクリメント)するように
+     * 最適化されてコンパイルされると、counterがループを終わるまで0になってしまう
+     * volatileをつけて宣言すれば解決する
+     * */
+    for(i = 0; i < MAX; i++)
+	counter ++;
+
+    printf("counter = %lld\n", counter);
+}
---
+++ signal/global-var2.s
@@ -0,0 +1,57 @@
+	.file	"global-var2.c"
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"%lld\n"
+	.text
+	.globl	handler
+	.type	handler, @function
+handler:
+.LFB14:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movq	counter(%rip), %rsi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE14:
+	.size	handler, .-handler
+	.section	.rodata.str1.1
+.LC1:
+	.string	"counter = %lld\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB15:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$handler, %esi
+	movl	$2, %edi
+	call	signal
+	movl	$1000000000, %eax
+.L5:
+	movq	counter(%rip), %rdx
+	addq	$1, %rdx
+	movq	%rdx, counter(%rip)
+	subq	$1, %rax
+	jne	.L5
+	movq	counter(%rip), %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE15:
+	.size	main, .-main
+	.local	counter
+	.comm	counter,8,8
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-44)"
+	.section	.note.GNU-stack,"",@progbits
